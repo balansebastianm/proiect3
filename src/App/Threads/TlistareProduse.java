ec56b5ce73688f18ea5d34cd9dd5df719ae295fb
balansebastianm <76501752+balansebastianm@users.noreply.github.com>
2021-11-03 01:46:43 +0200

    fixed thread

--- src/App/Threads/TlistareProduse.java
+++ src/App/Threads/TlistareProduse.java
@@ -6,14 +6,19 @@ import App.Produse.produseMagazin;
 import javax.swing.*;
 
 public class TlistareProduse extends adaugareInitiala implements Runnable{
+    produseMagazin[] p;
+    DefaultListModel<String> str;
+    int nrProd;
     public TlistareProduse(produseMagazin[] p, DefaultListModel<String> str, int nrProd){
+        this.nrProd = nrProd;
+        this.str = str;
+        this.p = p;
+    }
+    @Override
+    public void run() {
         for(int i=1; i<=nrProd; i++)
         {
             str.addElement("Produsul " + i + ": " + p[i].toString());
         }
-    }
-    @Override
-    public void run() {
-
     }
 }
